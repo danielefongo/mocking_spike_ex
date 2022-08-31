04534c18a115a631b12466314f403ea336da9ed6
danielefongo <danielefongo@gmail.com>
2022-08-31 12:52:10 +0200

    dinamically create Behaviour using macros

--- lib/mocking_spike.ex
+++ lib/mocking_spike.ex
@@ -1,11 +1,66 @@
+defmodule ConcreteBehaviour do
+  defmacro __using__(_opts) do
+    quote do
+      import Kernel, except: [@: 1]
+      require ConcreteBehaviour
+      import ConcreteBehaviour
+
+      Attributes.set(__MODULE__, [:specs], [])
+
+      @behaviour __MODULE__.Behaviour
+      @before_compile ConcreteBehaviour
+    end
+  end
+
+  defmacro __before_compile__(_) do
+    callbacks =
+      __CALLER__.module
+      |> Attributes.get([:specs])
+      |> Enum.map(fn a ->
+        quote do
+          @callback unquote(a)
+        end
+      end)
+
+    quote do
+      Kernel.defmodule(Behaviour, do: {:body, [], unquote(callbacks)})
+      Attributes.delete(__MODULE__, [:specs])
+    end
+  end
+
+  defmacro @({:spec, _, spec_body} = val) do
+    quote do
+      Attributes.update(
+        __MODULE__,
+        [:specs],
+        [],
+        &(&1 ++ unquote(Macro.escape(spec_body)))
+      )
+
+      Kernel.@(unquote(val))
+    end
+  end
+
+  defmacro @val do
+    quote do
+      Kernel.@(unquote(val))
+    end
+  end
+end
+
 defmodule CollaboratorBehaviour do
   @callback fun :: boolean()
 end
 
 defmodule Collaborator do
-  @behaviour CollaboratorBehaviour
+  use ConcreteBehaviour
+  import ConcreteBehaviour
+
   @spec fun :: boolean()
   def fun, do: true
+
+  @spec fun2 :: String.t()
+  def fun2, do: "Hello"
 end
 
 defmodule MockingSpike do
@@ -16,5 +71,10 @@ defmodule MockingSpike do
   @spec hello :: boolean()
   def hello do
     Collaborator.fun()
+  end
+
+  @spec hello2 :: String.t()
+  def hello2 do
+    Collaborator.fun2()
   end
 end
--- test/mocking_spike_test.exs
+++ test/mocking_spike_test.exs
@@ -6,11 +6,16 @@ defmodule MockingSpikeTest do
   import Rewire
   setup :verify_on_exit!
 
-  defmock(CollaboratorMock, for: CollaboratorBehaviour)
+  defmock(CollaboratorMock, for: Collaborator.Behaviour)
   rewire(MockingSpike, Collaborator: CollaboratorMock)
 
   test "foo" do
     expect(CollaboratorMock, :fun, fn -> true end)
     assert MockingSpike.hello() == true
+  end
+
+  test "foo2" do
+    expect(CollaboratorMock, :fun2, fn -> "Hi" end)
+    assert MockingSpike.hello2() == "Hi"
   end
 end
